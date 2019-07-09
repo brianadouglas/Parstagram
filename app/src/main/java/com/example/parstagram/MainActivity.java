0c50279eab9a85a5ef42a984c74770ac0aba140a
brianadouglas <briana.douglas@icloud.com>
2019-07-09 15:01:54 -0700

    first MVPs working

--- app/src/main/java/com/example/parstagram/MainActivity.java
+++ app/src/main/java/com/example/parstagram/MainActivity.java
@@ -1,40 +1,131 @@
 package com.example.parstagram;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SignUpCallback;
 
-    public ParseApplication app;
-    EditText user; // username entered
-    EditText pass; // password entered
+public class MainActivity extends AppCompatActivity implements TextWatcher {
+
+    private final String TAG = "Main";
+    EditText username; // username entered
+    EditText password; // password entered
     EditText email; // email address entered - only relevant for sign up
+    Button loginBtn;
+    Button signUpBtn;
+    public String persistUsername;
+    public String persistPassword;
+    boolean persisted = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // initialise tne Parse application
-        app = new ParseApplication();
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        email = (EditText) findViewById(R.id.email);
+
+        // setting up the text change listeners
+        username.addTextChangedListener(this);
+        password.addTextChangedListener(this);
+        email.addTextChangedListener(this);
+
+        loginBtn = (Button) findViewById(R.id.logIn);
+        signUpBtn = (Button) findViewById(R.id.create);
+
+        SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(this);
+        persistUsername = pref.getString("username", "n/a");
+        persistPassword = pref.getString("password", "n/a");
+
+        if (persistUsername != "n/a" && persistPassword != "n/a") {
+            persisted = true; // signifies that a username and password has been found from a previous session
+            login(findViewById(R.id.loginPage));
+        }
+
     }
 
     // on click for the log in button
     public void login (View v) {
-        user = (EditText) findViewById(R.id.username);
-        pass = (EditText) findViewById(R.id.password);
 
-        app.logIn(user.getText().toString(), pass.getText().toString());
+        // in the case that a login has not persisted, pull from the EditText input
+        if (!persisted) {
+            persistUsername = username.getText().toString();
+            persistPassword = password.getText().toString();
+        }
+
+        ParseUser.logInInBackground(persistUsername, persistPassword, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                if (user != null) {
+                    // Successful login
+                    Toast.makeText(MainActivity.this, "Successful login", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Failure
+                    Log.e(TAG, "Failed log in", e);
+                }
+            }
+        });
     }
 
     // on click for the sign up button
-    public void signup (View v) {
-        user = (EditText) findViewById(R.id.username);
-        pass = (EditText) findViewById(R.id.password);
-        email = (EditText) findViewById(R.id.email);
+    public void signUp (View v) {
+        // Create the ParseUser
+        ParseUser user = new ParseUser();
+        // Set core properties
+        user.setUsername(username.getText().toString());
+        user.setPassword(password.getText().toString());
+        user.setEmail(email.getText().toString());
+        // Implement sign up in the background
+        user.signUpInBackground(new SignUpCallback() {
+            @Override
+            public void done(ParseException e) {
+                if (e == null) {
+                    // Successful login
+                    // TODO: continue onto the relevant activity
+                    Toast.makeText(MainActivity.this, "Successful sign up", Toast.LENGTH_SHORT).show();
+                } else {
+                    // Unsuccessful - could remain on the main activity
+                    Log.e(TAG, "Failed sign up", e);
+                }
+            }
+        });
+    }
 
-        app.signUp(user.getText().toString(), pass.getText().toString(), email.getText().toString());
+    public void logOut() {
+        ParseUser.logOut();
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+        // enables the buttons based on which fields are filled in
+        if (username.getText().length() > 0 && password.getText().length() > 0) {
+            loginBtn.setEnabled(true);
+            if (email.getText().length() > 0) {
+                signUpBtn.setEnabled(true);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/parstagram/ParseApplication.java
+++ app/src/main/java/com/example/parstagram/ParseApplication.java
@@ -1,13 +1,8 @@
 package com.example.parstagram;
 
 import android.app.Application;
-import android.util.Log;
 
-import com.parse.LogInCallback;
 import com.parse.Parse;
-import com.parse.ParseException;
-import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
@@ -37,54 +32,11 @@ public class ParseApplication extends Application {
                 .applicationId("dorraine-2019") // should correspond to APP_ID env variable
                 .clientKey(null)  // set explicitly unless clientKey is explicitly configured on Parse server
                 .clientBuilder(builder)
-                .server("http://brianadouglas-fbu-instagram.herokuapp.com/parse").build());
+                .server("https://brianadouglas-fbu-instagram.herokuapp.com/parse/").build());
 
-        // New test creation of object below
 //        ParseObject testObject = new ParseObject("TestObject");
 //        testObject.put("foo", "bar");
 //        testObject.saveInBackground();
-    }
-
-    // allowing user to sign up for a new account using Parse
-    public void signUp(String username, String password, String email) {
-        // create the ParseUser
-        ParseUser user = new ParseUser();
-        // Set core properties
-        user.setUsername(username); // take these values from the user
-        user.setPassword(password); // also from the user
-        user.setEmail(email);
-        // Invoke signUpInBackground
-        user.signUpInBackground(new SignUpCallback() {
-            public void done(ParseException e) {
-                if (e == null) {
-                    // Hooray! Let them use the app now.
-                    Log.d("SIGN UP", "Successful login");
-                } else {
-                    // Sign up didn't succeed. Look at the ParseException
-                    // to figure out what went wrong
-                    Log.d("SIGN UP", "Unsuccessful login");
-                }
-            }
-        });
-    }
-
-    // allow the user to log in
-    public void logIn(String username, String password) {
-        ParseUser.logInInBackground(username, password, new LogInCallback() {
-            public void done(ParseUser user, ParseException e) {
-                if (user != null) {
-                    Log.d("LOG IN", "Successful login");
-                } else {
-                    // Signup failed. Look at the ParseException to see what happened.
-                    Log.d("LOG IN", "Unsuccessful login");
-                }
-            }
-        });
-    }
-
-    // allow the user to log out
-    public void logOut() {
-        ParseUser.logOut();
     }
 
 }
